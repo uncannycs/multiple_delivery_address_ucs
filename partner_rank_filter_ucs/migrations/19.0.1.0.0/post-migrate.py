f906b603d5c6ab73aa288cce5bc00893c713afcd
ak-ucs <akodooucs@gmail.com>
2026-08-31 16:23:02 +0530

    Delete partner_rank_filter_ucs directory

--- partner_rank_filter_ucs/migrations/19.0.1.0.0/post-migrate.py
+++
@@ -1,9 +0,0 @@
-"""Post-migrate customer_vendor_ucs 17.x → 19.0.1.0.0."""
-import logging
-
-_logger = logging.getLogger(__name__)
-
-
-def migrate(cr, version):
-    if version:
-        _logger.info("Post-migrate customer_vendor_ucs from %s", version)
--- partner_rank_filter_ucs/models/purchase_order.py
+++
@@ -1,8 +0,0 @@
-from odoo import fields, models
-
-class PurchaseOrder(models.Model):
-    _inherit = 'purchase.order'
-
-
-    partner_id = fields.Many2one('res.partner', domain="['|', ('company_id', '=', False), ('company_id', '=', company_id), ('supplier_rank', '>', 0)]")
-    customer_id = fields.Many2one('res.partner', string="Customer")
